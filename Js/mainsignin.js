ae1c91ee9f916c29f36fbd697fb680ea180a2be6
Deepak <hakNinja@gmail.com>
2021-05-15 12:03:06 +0530

    random images

--- Js/mainsignin.js
+++ Js/mainsignin.js
@@ -33,26 +33,6 @@ function closeLogin(){
   document.getElementById("buttondiv").style.display="none";
 }
 
-function randImage(){
-  var files = [            
-    "../Homepageimages/mySticker/1.png",
-    "../Homepageimages/mySticker/2.png",
-    "../Homepageimages/mySticker/3.png",
-    "../Homepageimages/mySticker/4.png",
-    "../Homepageimages/mySticker/5.png",
-    "../Homepageimages/mySticker/6.png",
-    "../Homepageimages/mySticker/7.png",
-    "../Homepageimages/mySticker/8.png",
-    "../Homepageimages/mySticker/9.png",
-    "../Homepageimages/mySticker/10.png",
-    "../Homepageimages/mySticker/11.png"];
-    var randIndex = Math.floor(Math.random() * files.length);
-    document.getElementById("imagesslideshowdiv").style.backgroundImage = "url('" + files[randIndex] + "')";
-    document.getElementById("imagesslideshowdiv").style.backgroundRepeat="no-repeat";
-    document.getElementById("imagesslideshowdiv").style.backgroundSize="auto 100%";
-  }
-randImage();
-setInterval(randImage, 5000);
 //run at start
 document.getElementById("studentsigninform").style.display="none";
 document.getElementById("teachersigninform").style.display="none";
